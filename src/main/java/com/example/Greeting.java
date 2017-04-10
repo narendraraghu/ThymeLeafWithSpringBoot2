ae54c21ab54a111b0a16a9bd191c3559db9d9342
narendraraghu <Narendra@89>
2017-04-10 17:58:53 +0530

    Revert all the unneceesory changes

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -7,7 +7,27 @@ package com.example;
 public class Greeting {
 
     private long id;
+    private String name;
+    private String email;
     private String content;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 
     public long getId() {
         return id;
